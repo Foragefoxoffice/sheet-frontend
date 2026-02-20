03208ab479a50740d0780f99e023a5115130c8c9
Foragefoxoffice <actewpdeveloper1@gmail.com>
2026-02-20 15:34:44 +0530

    sdsd

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
- const API_URL = 'https://placetest.in/api';
+const API_URL = 'https://placetest.in/api';
 // const API_URL = 'http://localhost:5003/api';
 
 const api = axios.create({
