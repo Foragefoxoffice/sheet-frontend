7213b333314c9b4a7dfcf42d5d95b21b62dbae49
Foragefoxoffice <actewpdeveloper1@gmail.com>
2026-02-21 18:25:55 +0530

    .env push

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://placetest.in/api';
-// const API_URL = 'http://localhost:5003/api';
+const API_URL = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : 'http://localhost:5003/api';
 
 const api = axios.create({
     baseURL: API_URL,
